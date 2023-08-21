71d9b175c41e6244c4f7c32e89f2f6c01b3e8dcf
RishatMamat <reshatma@gmail.com>
2023-08-20 18:56:55 -0600

    Add workflow tests

--- src/tests/single_endpoints/common_options/all_crocodile_options.ts
+++ src/tests/single_endpoints/common_options/all_crocodile_options.ts
@@ -12,3 +12,18 @@ export const options = {
     http_req_duration: ["p(95) < 200"],
   },
 };
+
+export const workflowOptions = {
+  discardResponseBodies: true,
+  scenarios: {
+    all_crocodile: {
+      executor: "constant-vus",
+      vus: 5,
+      duration: "10s",
+    },
+  },
+  thresholds: {
+    http_req_failed: ["rate < 0.01"],
+    http_req_duration: ["p(90) < 5000"],
+  },
+};
---
+++ src/tests/user_flow_endpoints/smoke/create_update_get_delete.test.ts
@@ -0,0 +1,84 @@
+import http from "k6/http";
+import { check, group, sleep } from "k6";
+import { workflowOptions as options } from "../../single_endpoints/common_options/all_crocodile_options";
+import { TestType, getConfig } from "../../../../config";
+import { workFlowUser } from "../../../test_data/user/existingTestUser";
+
+export { options };
+
+let testType = "api" as TestType;
+let config = getConfig(testType);
+
+const userLoginInformation = {
+  username: workFlowUser.username,
+  password: workFlowUser.password,
+};
+
+const loginHeaders = { headers: { "Content-Type": "application/json" } };
+
+let token: any;
+
+export default function () {
+  group("Log in with the workflow user", function () {
+    let loginResponse = http.post(
+      `${config.baseUrl}/auth/token/login/`,
+      JSON.stringify(userLoginInformation),
+      loginHeaders
+    );
+
+    check(loginResponse, {
+      "response status": (r) => r.status === 200,
+    });
+
+    sleep(1);
+
+    token = loginResponse.json("access");
+
+    const authHeaders = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    };
+  });
+
+  //   group("Create a corcodile with the use", function () {
+  //     const response = http.get(
+  //       `${config.baseUrl}/public/crocodiles/${randomCrocodileId}`
+  //     );
+
+  //     check(response, {
+  //       "response status": (r) => r.status === 200,
+  //     });
+  //   });
+
+  //   sleep(1);
+
+  //   group("Update the created crocodile", function () {
+  //     const response = http.get(`${config.baseUrl}/public/crocodiles/`);
+
+  //     check(response, {
+  //       "response status": (r) => r.status === 200,
+  //     });
+  //   });
+
+  //   sleep(1);
+
+  //   group("Get the created and updated crocodile", function () {
+  //     const response = http.get(`${config.baseUrl}/public/crocodiles/`);
+
+  //     check(response, {
+  //       "response status": (r) => r.status === 200,
+  //     });
+  //   });
+
+  //   sleep(1);
+
+  //   group("Delete the created crocodile", function () {
+  //     const response = http.get(`${config.baseUrl}/public/crocodiles/`);
+
+  //     check(response, {
+  //       "response status": (r) => r.status === 200,
+  //     });
+  //   });
+}
